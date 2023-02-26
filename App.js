dcdd3ec2ab39b5a337e463a47826374a02840acc
Dubjay18 <jejeniyi7@gmail.com>
2023-02-26 19:32:22 +0100

    fix removed dotenv packaage

--- App.js
+++ App.js
@@ -7,8 +7,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from "react-query";
-import dotenv from "dotenv";
-dotenv.config();
+
 export default function App() {
   const queryClient = new QueryClient();
   return (
